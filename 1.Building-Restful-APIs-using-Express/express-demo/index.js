2817ee939dd78af9a5fc6127925005d83412e8fa
FirmanJabar <firmanabduljabar@gmail.com>
2020-01-11 13:23:26 +0800

    :art: 6. Environment Variables

--- 1.Building-Restful-APIs-using-Express/express-demo/index.js
+++ 1.Building-Restful-APIs-using-Express/express-demo/index.js
@@ -3,11 +3,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello Firman</h1>');
+    res.send('<h1>Hello Firman Abdul Jabar</h1>');
 });
 
 app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3, 4, 5]);
 });
 
-app.listen(3000, () => console.log('Listening on Port 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on Port ${port}`));
