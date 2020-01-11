876cad219dc0c87abe6e650cfaa4cb8482dc0c1a
FirmanJabar <firmanabduljabar@gmail.com>
2020-01-11 13:48:50 +0800

    :pencil: 8. Handling HTTP GET Requests

--- 1.Building-Restful-APIs-using-Express/express-demo/index.js
+++ 1.Building-Restful-APIs-using-Express/express-demo/index.js
@@ -2,16 +2,36 @@ const express = require('express');
 
 const app = express();
 
+const courses = [{
+        id: 1,
+        name: 'course 1'
+    },
+    {
+        id: 2,
+        name: 'course 2'
+    },
+    {
+        id: 3,
+        name: 'course 3'
+    },
+    {
+        id: 4,
+        name: 'course 4'
+    },
+];
+
 app.get('/', (req, res) => {
     res.send('<h1>Hello Firman Abdul Jabar</h1>');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3, 4, 5]);
+    res.send(courses);
 });
 
-app.get('/api/post/:year/:month', (req, res) => {
-    res.send(req.query);
+app.get('/api/courses/:id', (req, res) => {
+    var course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('Course not Found!');
+    res.send(course);
 });
 
 const port = process.env.PORT || 3000;
