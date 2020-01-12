0d210bff4a64542898c14908ea29cf781a90132b
FirmanJabar <firmanabduljabar@gmail.com>
2020-01-12 09:22:52 +0800

    :pencil: 13. Handling HTTP Delete Requests

--- 1.Building-Restful-APIs-using-Express/express-demo/index.js
+++ 1.Building-Restful-APIs-using-Express/express-demo/index.js
@@ -34,10 +34,7 @@ app.post('/api/courses', (req, res) => {
     const {
         error
     } = validateCourse(req.body);
-    if (error) {
-        res.status(400).send(error.details[0].message);
-        return
-    }
+    if (error) return res.status(400).send(error.details[0].message);
 
     const course = {
         id: courses.length + 1,
@@ -49,15 +46,12 @@ app.post('/api/courses', (req, res) => {
 
 app.put('/api/courses/:id', (req, res) => {
     var course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('Course not Found!');
+    if (!course) return res.status(404).send('Course not Found!');
 
     const {
         error
     } = validateCourse(req.body);
-    if (error) {
-        res.status(400).send(error.details[0].message);
-        return
-    }
+    if (error) return res.status(400).send(error.details[0].message);
 
     course.name = req.body.name;
     res.send(course);
@@ -65,8 +59,18 @@ app.put('/api/courses/:id', (req, res) => {
 
 app.get('/api/courses/:id', (req, res) => {
     var course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('Course not Found!');
+    if (!course) return res.status(404).send('Course not Found!');
     res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    var course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('Course not Found!');
+
+    const del = courses.indexOf(course);
+    courses.splice(del, 1);
+
+    res.send(courses);
 });
 
 const port = process.env.PORT || 3000;
