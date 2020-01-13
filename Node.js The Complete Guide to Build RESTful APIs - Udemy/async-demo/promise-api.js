6d1889cdecda6b01dcaf00c5b90183a97b971a2f
FirmanJabar <firmanabduljabar@gmail.com>
2020-01-13 18:51:21 +0800

    :pencil: 10. Running Parallel Promises

--- Node.js The Complete Guide to Build RESTful APIs - Udemy/async-demo/promise-api.js
+++ Node.js The Complete Guide to Build RESTful APIs - Udemy/async-demo/promise-api.js
@@ -1,9 +1,33 @@
-// unit testing
-const p = Promise.resolve({
-    id: 1,
-    name: 'firman'
-})
-p.then(user => console.log(user));
+const p1 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('get api from facebook');
+        resolve({
+            from: 'Facebook',
+            link: 'facebook.com',
+            address: {
+                city: 'cicaheum',
+                country: 'indonesia'
+            }
+        });
+    }, 5000);
+});
+const p2 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('get api from Twitter');
+        resolve({
+            from: 'Twitter',
+            link: 'Twitter.com',
+            address: {
+                city: 'miami',
+                country: 'US'
+            }
+        });
+    }, 2000);
+});
 
-const q = Promise.reject(new Error('Write error in here!'))
-q.catch(err => console.log(err));
+//Jika menggunakan `Promise.all()`, salah satu promise ada yg error
+//Maka semua promise akan mengembalikan nilai error (tidak ada nilai yg diresolve)
+//Gunakan `Promise.race()`, jika ingin mengambil satu data tercepat.
+Promise.all([p1, p2]).then(result => {
+    console.log(result);
+})
