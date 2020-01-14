007ffd9640129758ae2d0decd746d85af372f2d1
FirmanJabar <firmanabduljabar@gmail.com>
2020-01-14 16:53:45 +0800

    :pencil: 9. Comparison Query Operators

--- Node.js The Complete Guide to Build RESTful APIs - Udemy/mongo-demo/index.js
+++ Node.js The Complete Guide to Build RESTful APIs - Udemy/mongo-demo/index.js
@@ -36,15 +36,50 @@ async function createCourse() {
 }
 
 async function getCourses() {
-    const courses = await Course.find({
-        author: 'Firman Abdul Jabar',
-        isPublish: true
-    }).limit(10).sort({
-        name: 1
-    }).select({
-        name: 1,
-        tags: 1
-    })
+    //operator for queries:
+    //eq = equal
+    //ne = not equal
+    //gt = greater than
+    //gte = greater than or equal
+    //lt = less than
+    //lte = less than or equal
+    //in = digunkan untuk mencari beberapa data misal: harga $10,$20 dan $30
+    //nin = not in
+    const courses = await Course
+        // .find({
+        //     author: 'Firman Abdul Jabar',
+        //     isPublish: true
+        // })
+
+        //mencari harga lebih dari sama dengan $10
+        .find({
+            price: {
+                $gte: 10
+            }
+        })
+
+        //mencari harga $10 s.d. $30
+        .find({
+            price: {
+                $gte: 10,
+                $lte: 30
+            }
+        })
+
+        //in = digunkan untuk mencari beberapa data misal: harga $10,$20 dan $30
+        .find({
+            price: {
+                $in: [10, 20, 30]
+            }
+        })
+
+        .limit(10).sort({
+            name: 1
+        })
+        .select({
+            name: 1,
+            tags: 1
+        })
     console.log(courses);
 }
 
