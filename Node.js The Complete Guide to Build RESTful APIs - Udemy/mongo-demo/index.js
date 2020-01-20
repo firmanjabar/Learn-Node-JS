afd03a0ffc8ac3b2855ab00a1da803c96e402d5b
FirmanJabar <firmanabduljabar@gmail.com>
2020-01-20 10:12:14 +0800

    :mag: 2. Built-In Validators

--- Node.js The Complete Guide to Build RESTful APIs - Udemy/mongo-demo/index.js
+++ Node.js The Complete Guide to Build RESTful APIs - Udemy/mongo-demo/index.js
@@ -1,76 +1,99 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/playground', {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        useFindAndModify: false,
-    })
-    .then(() => {
-        console.log('connected to mongodb')
-    })
-    .catch(err => {
-        console.error('Cant connect to mongodb', err)
-    });
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost/playground", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch(err => {
+    console.error("Cant connect to mongodb", err);
+  });
 
 const courseSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255
+    // match: /patterRegex/
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ["web", "android", "networking"]
+  },
+  author: String,
+  tags: [String],
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  isPublish: Boolean,
+  price: {
+    type: Number,
+    required: function() {
+      return this.isPublish;
     },
-    author: String,
-    tags: [String],
-    date: {
-        type: Date,
-        default: Date.now
-    },
-    isPublish: Boolean
+    min: 10,
+    max: 100
+  }
 });
 
-const Course = mongoose.model('Course', courseSchema);
+const Course = mongoose.model("Course", courseSchema);
 
 async function createCourse() {
-    const course = new Course({
-        // name: 'ReactJS Tutorial',
-        author: 'Carson Fanicos',
-        tags: ['frontend', 'spa'],
-        isPublish: true
-    })
+  const course = new Course({
+    name: "Bootstrap Tutorial",
+    category: "web",
+    author: "Emilia Clarke",
+    tags: ["frontend", "css"],
+    isPublish: true,
+    price: 102
+  });
 
-    try {
-        const result = await course.save();
-        console.log(result);
-    } catch (err) {
-        console.log(err.message);
-    }
+  try {
+    const result = await course.save();
+    console.log(result);
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
 async function getCourses() {
-    const courses = await Course
-        .find({
-            author: 'Firman Abdul Jabar',
-            isPublish: true
-        })
-        .limit(10).sort({
-            name: 1
-        })
-        .count()
-    console.log(courses);
+  const courses = await Course.find({
+    author: "Firman Abdul Jabar",
+    isPublish: true
+  })
+    .limit(10)
+    .sort({
+      name: 1
+    })
+    .count();
+  console.log(courses);
 }
 
 async function getUpdateCourse(id) {
-    const course = await Course.findByIdAndUpdate(id, {
-        $set: {
-            author: 'Carson Fanicos',
-            isPublish: true
-        }
-    }, {
-        new: true
-    });
-    console.log(course);
+  const course = await Course.findByIdAndUpdate(
+    id,
+    {
+      $set: {
+        author: "Carson Fanicos",
+        isPublish: true
+      }
+    },
+    {
+      new: true
+    }
+  );
+  console.log(course);
 }
 
 async function removeCourse(id) {
-    const course = await Course.findByIdAndRemove(id);
-    console.log(course);
+  const course = await Course.findByIdAndRemove(id);
+  console.log(course);
 }
 
 createCourse();
